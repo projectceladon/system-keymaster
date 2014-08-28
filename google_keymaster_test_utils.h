83804621f399f4bcab9281c0eecfcbefe56b054e
Shawn Willden <swillden@google.com>
2014-08-28 13:47:40 -0600

    Add more logging methods, to distinguish message priorities.
    Change-Id: I2308af04eb699fb896d3e701a659945451304ddf

--- google_keymaster_test_utils.h
+++ google_keymaster_test_utils.h
@@ -99,6 +99,30 @@ class StdoutLogger : public Logger {
         va_end(args);
         return result;
     }
+
+    int info(const char* fmt, ...) const {
+        va_list args;
+        va_start(args, fmt);
+        int result = vprintf(fmt, args);
+        va_end(args);
+        return result;
+    }
+
+    int error(const char* fmt, ...) const {
+        va_list args;
+        va_start(args, fmt);
+        int result = vfprintf(stderr, fmt, args);
+        va_end(args);
+        return result;
+    }
+
+    int severe(const char* fmt, ...) const {
+        va_list args;
+        va_start(args, fmt);
+        int result = vfprintf(stderr, fmt, args);
+        va_end(args);
+        return result;
+    }
 };
 
 }  // namespace test
