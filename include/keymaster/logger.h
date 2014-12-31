538b0654fd5096841e12da15271c74429a37be18
Shawn Willden <swillden@google.com>
2014-12-30 23:23:40 -0700

    Refactor logging, to stop passing Logger references everywhere.
    Change-Id: I9380c21872710743413ca6a4340ae19f58b1e983

--- include/keymaster/logger.h
+++ include/keymaster/logger.h
@@ -17,22 +17,74 @@
 #ifndef SYSTEM_KEYMASTER_LOGGER_H_
 #define SYSTEM_KEYMASTER_LOGGER_H_
 
+#include <stdarg.h>
+
 namespace keymaster {
 
 class Logger {
   public:
     Logger() {}
     virtual ~Logger() {}
-    virtual int debug(const char* fmt, ...) const = 0;
-    virtual int info(const char* fmt, ...) const = 0;
-    virtual int error(const char* fmt, ...) const = 0;
-    virtual int severe(const char* fmt, ...) const = 0;
+
+    enum LogLevel {
+        DEBUG_LVL,    // Messages used only for debugging
+        INFO_LVL,     // Informational messages; something is unusual but not wrong
+        WARNING_LVL,  // There's an indication of trouble, but it may be okay.
+        ERROR_LVL,    // A problem has occurred, but processing can continue
+        SEVERE_LVL,   // A severe problem has occurred; likely indicates a defect.
+    };
+
+    virtual int log_msg(LogLevel level, const char* fmt, va_list args) const = 0;
+
+    // These methods are deprecated.  Use the static methods or, better yet, the macros.
+    int log(LogLevel level, const char* fmt, ...) const;
+    int debug(const char* fmt, ...) const;
+    int info(const char* fmt, ...) const;
+    int warning(const char* fmt, ...) const;
+    int error(const char* fmt, ...) const;
+    int severe(const char* fmt, ...) const;
+
+    static int Log(LogLevel level, const char* fmt, va_list args);
+    static int Log(LogLevel level, const char* fmt, ...);
+    static int Debug(const char* fmt, ...);
+    static int Info(const char* fmt, ...);
+    static int Warning(const char* fmt, ...);
+    static int Error(const char* fmt, ...);
+    static int Severe(const char* fmt, ...);
+
+  protected:
+    static void set_instance(Logger* logger) { instance_ = logger; }
 
   private:
     // Disallow copying.
     Logger(const Logger&);
     void operator=(const Logger&);
+
+    static Logger* instance_;
 };
+
+#ifdef DEBUG
+
+#define STR(x) #x
+#define STRINGIFY(x) STR(x)
+#define FILE_LINE __FILE__ ":" STRINGIFY(__LINE__) ": "
+
+#define LOG_D(fmt, ...) Logger::Debug(FILE_LINE fmt, __VA_ARGS__)
+#define LOG_I(fmt, ...) Logger::Info(FILE_LINE fmt, __VA_ARGS__)
+
+#else  // not DEBUG
+
+// For production builds, don't bloat the executable with FILE_LINE strings or DEBUG or INFO log
+// messages.
+#define FILE_LINE
+#define LOG_D(fmt, ...)
+#define LOG_I(fmt, ...)
+
+#endif  // not DEBUG
+
+#define LOG_W(fmt, ...) Logger::Warning(FILE_LINE fmt, __VA_ARGS__)
+#define LOG_E(fmt, ...) Logger::Error(FILE_LINE fmt, __VA_ARGS__)
+#define LOG_S(fmt, ...) Logger::Severe(FILE_LINE fmt, __VA_ARGS__)
 
 }  // namespace keymaster
 
