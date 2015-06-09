34419130408d2a6dcadd7b0f1b6d2c9c4002bbac
Shawn Willden <swillden@google.com>
2015-06-08 23:10:44 -0600

    GCM tags in ciphertext, rather than in params.
    Also, handle AAD correctly.
    
    Bug: 21786749
    Change-Id: I26a413f39daf3bd946ed494c7c3b5c6f559fb30b

--- aes_operation.h
+++ aes_operation.h
@@ -19,21 +19,52 @@
 
 #include <openssl/evp.h>
 
-#include "aead_mode_operation.h"
 #include "ocb_utils.h"
 #include "operation.h"
 
 namespace keymaster {
+
+/**
+ * Abstract base for AES operation factories.  This class does all of the work to create
+ * AES operations.
+ */
+class AesOperationFactory : public OperationFactory {
+  public:
+    KeyType registry_key() const override { return KeyType(KM_ALGORITHM_AES, purpose()); }
+
+    Operation* CreateOperation(const Key& key, const AuthorizationSet& begin_params,
+                               keymaster_error_t* error) override;
+    const keymaster_block_mode_t* SupportedBlockModes(size_t* block_mode_count) const override;
+    const keymaster_padding_t* SupportedPaddingModes(size_t* padding_count) const override;
+
+    virtual keymaster_purpose_t purpose() const = 0;
+};
+
+/**
+ * Concrete factory for AES encryption operations.
+ */
+class AesEncryptionOperationFactory : public AesOperationFactory {
+    keymaster_purpose_t purpose() const override { return KM_PURPOSE_ENCRYPT; }
+};
+
+/**
+ * Concrete factory for AES decryption operations.
+ */
+class AesDecryptionOperationFactory : public AesOperationFactory {
+    keymaster_purpose_t purpose() const override { return KM_PURPOSE_DECRYPT; }
+};
 
 static const size_t MAX_EVP_KEY_SIZE = 32;
 
 class AesEvpOperation : public Operation {
   public:
     AesEvpOperation(keymaster_purpose_t purpose, keymaster_block_mode_t block_mode,
-                    keymaster_padding_t padding, bool caller_iv, const uint8_t* key,
-                    size_t key_size);
+                    keymaster_padding_t padding, bool caller_iv, size_t tag_length,
+                    const uint8_t* key, size_t key_size);
     ~AesEvpOperation();
 
+    keymaster_error_t Begin(const AuthorizationSet& input_params,
+                            AuthorizationSet* output_params) override;
     keymaster_error_t Update(const AuthorizationSet& additional_params, const Buffer& input,
                              AuthorizationSet* output_params, Buffer* output,
                              size_t* input_consumed) override;
@@ -47,12 +78,22 @@ class AesEvpOperation : public Operation {
     bool need_iv() const;
     keymaster_error_t InitializeCipher();
     keymaster_error_t GetIv(const AuthorizationSet& input_params);
+    bool HandleAad(const AuthorizationSet& input_params, const Buffer& input,
+                   keymaster_error_t* error);
+    bool ProcessAadBlocks(const uint8_t* data, size_t blocks, keymaster_error_t* error);
+    void FillBufferedAadBlock(keymaster_blob_t* aad);
+    bool ProcessBufferedAadBlock(keymaster_error_t* error);
+    bool InternalUpdate(const uint8_t* input, size_t input_length, Buffer* output,
+                        keymaster_error_t* error);
 
     const keymaster_block_mode_t block_mode_;
     EVP_CIPHER_CTX ctx_;
     UniquePtr<uint8_t[]> iv_;
     size_t iv_length_;
     const bool caller_iv_;
+    size_t tag_length_;
+    UniquePtr<uint8_t[]> aad_block_buf_;
+    size_t aad_block_buf_length_;
 
   private:
     bool data_started_;
@@ -65,8 +106,8 @@ class AesEvpEncryptOperation : public AesEvpOperation {
   public:
     AesEvpEncryptOperation(keymaster_block_mode_t block_mode, keymaster_padding_t padding,
                            bool caller_iv, size_t tag_length, const uint8_t* key, size_t key_size)
-        : AesEvpOperation(KM_PURPOSE_ENCRYPT, block_mode, padding, caller_iv, key, key_size),
-          tag_length_(tag_length) {}
+        : AesEvpOperation(KM_PURPOSE_ENCRYPT, block_mode, padding, caller_iv, tag_length, key,
+                          key_size) {}
 
     keymaster_error_t Begin(const AuthorizationSet& input_params,
                             AuthorizationSet* output_params) override;
@@ -77,64 +118,34 @@ class AesEvpEncryptOperation : public AesEvpOperation {
 
   private:
     keymaster_error_t GenerateIv();
-
-    size_t tag_length_;
 };
 
 class AesEvpDecryptOperation : public AesEvpOperation {
   public:
     AesEvpDecryptOperation(keymaster_block_mode_t block_mode, keymaster_padding_t padding,
-                           const uint8_t* key, size_t key_size)
+                           size_t tag_length, const uint8_t* key, size_t key_size)
         : AesEvpOperation(KM_PURPOSE_DECRYPT, block_mode, padding,
-                          false /* caller_iv -- don't care */, key, key_size),
-          tag_provided_(false) {}
+                          false /* caller_iv -- don't care */, tag_length, key, key_size) {}
 
     keymaster_error_t Begin(const AuthorizationSet& input_params,
                             AuthorizationSet* output_params) override;
     keymaster_error_t Update(const AuthorizationSet& additional_params, const Buffer& input,
                              AuthorizationSet* output_params, Buffer* output,
                              size_t* input_consumed) override;
+    keymaster_error_t Finish(const AuthorizationSet& additional_params, const Buffer& signature,
+                             AuthorizationSet* output_params, Buffer* output) override;
 
     int evp_encrypt_mode() override { return 0; }
 
   private:
-    bool tag_provided_;
-};
+    size_t tag_buf_unused() { return tag_length_ - tag_buf_length_; }
 
-/**
- * Abstract base for AES operation factories.  This class does all of the work to create
- * AES operations.
- */
-class AesOperationFactory : public OperationFactory {
-  public:
-    virtual KeyType registry_key() const { return KeyType(KM_ALGORITHM_AES, purpose()); }
+    keymaster_error_t ProcessAllButTagLengthBytes(const Buffer& input, Buffer* output);
+    bool ProcessTagBufContentsAsData(size_t to_process, Buffer* output, keymaster_error_t* error);
+    void BufferCandidateTagData(const uint8_t* data, size_t data_length);
 
-    virtual Operation* CreateOperation(const Key& key, const AuthorizationSet& begin_params,
-                                       keymaster_error_t* error);
-    virtual const keymaster_block_mode_t* SupportedBlockModes(size_t* block_mode_count) const;
-    virtual const keymaster_padding_t* SupportedPaddingModes(size_t* padding_count) const;
-
-    virtual keymaster_purpose_t purpose() const = 0;
-
-  private:
-    virtual Operation* CreateEvpOperation(const SymmetricKey& key,
-                                          keymaster_block_mode_t block_mode,
-                                          keymaster_padding_t padding, bool caller_iv,
-                                          size_t tag_length, keymaster_error_t* error);
-};
-
-/**
- * Concrete factory for AES encryption operations.
- */
-class AesEncryptionOperationFactory : public AesOperationFactory {
-    keymaster_purpose_t purpose() const { return KM_PURPOSE_ENCRYPT; }
-};
-
-/**
- * Concrete factory for AES decryption operations.
- */
-class AesDecryptionOperationFactory : public AesOperationFactory {
-    keymaster_purpose_t purpose() const { return KM_PURPOSE_DECRYPT; }
+    UniquePtr<uint8_t[]> tag_buf_;
+    size_t tag_buf_length_;
 };
 
 }  // namespace keymaster
