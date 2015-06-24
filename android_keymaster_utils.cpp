b9f3abbd210647b7c2a9a0341119fa609ee08d9c
Shawn Willden <swillden@google.com>
2015-06-24 13:23:11 -0700

    Limit dup_buffer to 16 MiB allocations.
    Bug: 21888473
    Change-Id: I14c658f5c57bd551e4d136b7d6146b8efdfacf27

--- android_keymaster_utils.cpp
+++ android_keymaster_utils.cpp
@@ -20,7 +20,13 @@
 
 namespace keymaster {
 
+// Keymaster never manages enormous buffers, so anything particularly large is bad data or the
+// result of a bug.  We arbitrarily set a 16 MiB limit.
+const size_t kMaxDupBufferSize = 16 * 1024 * 1024;
+
 uint8_t* dup_buffer(const void* buf, size_t size) {
+    if (size >= kMaxDupBufferSize)
+        return nullptr;
     uint8_t* retval = new (std::nothrow) uint8_t[size];
     if (retval)
         memcpy(retval, buf, size);
