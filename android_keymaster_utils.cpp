fc3c1eff0940f9cc92c5a1528b1ae33e7985903f
Chad Brubaker <cbrubaker@google.com>
2015-08-13 17:57:30 -0700

    Fix benign unsigned overflow in memcmp_s
    Change-Id: I7e87c958d5998640863f8c40ff1ec0454ff0d5e5

--- android_keymaster_utils.cpp
+++ android_keymaster_utils.cpp
@@ -37,8 +37,10 @@ int memcmp_s(const void* p1, const void* p2, size_t length) {
     const uint8_t* s1 = static_cast<const uint8_t*>(p1);
     const uint8_t* s2 = static_cast<const uint8_t*>(p2);
     uint8_t result = 0;
-    while (length-- > 0)
+    while (length > 0) {
+        length--;
         result |= *s1++ ^ *s2++;
+    }
     return result == 0 ? 0 : 1;
 }
 
