b87d707b96e6af9dabd23481b5c7a51160630831
Shawn Willden <swillden@google.com>
2015-11-25 07:53:13 -0700

    Set SoftKeymasterDevice flags correctly.
    Bug: 25166155
    Change-Id: Ibaf0ef42744094b2dde26539e8f51d381061eb1e

--- include/keymaster/soft_keymaster_device.h
+++ include/keymaster/soft_keymaster_device.h
@@ -78,7 +78,7 @@ class SoftKeymasterDevice {
     }
 
   private:
-    void initialize_device_struct();
+    void initialize_device_struct(uint32_t flags);
     bool FindUnsupportedDigest(keymaster_algorithm_t algorithm, keymaster_purpose_t purpose,
                                const AuthorizationSet& params,
                                keymaster_digest_t* unsupported) const;
